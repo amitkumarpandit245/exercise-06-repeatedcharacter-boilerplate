6827507f75eef5b1149723d05c0052d6cf8de4d0
Ubuntu <ubuntu@ip-172-31-41-20.ec2.internal>
2019-11-11 03:50:44 +0000

    Modified

--- src/main/java/com/stackroute/RepeatedCharacter.java
+++ src/main/java/com/stackroute/RepeatedCharacter.java
@@ -1,12 +1,7 @@
 package com.stackroute;
-
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Scanner;
-import java.util.stream.Stream;
-
 public class RepeatedCharacter {
 
    	//Do not print anything other than what's expected/asked in problem
@@ -14,36 +9,44 @@ public class RepeatedCharacter {
    	//Use Scanner to get input from console
     	Scanner scanner=new Scanner(System.in);
     	String input=scanner.nextLine();
-    	RepeatedCharacter.findRepeatedCharacter(input);
+    	Character result=RepeatedCharacter.findRepeatedCharacter(input);
+    	System.out.println(result);
+    	scanner.close();
 	}
 
 	public static Character findRepeatedCharacter(String input) {
-		Character res=0;
-		Map<Character,Integer> count=new HashMap<>();
-		if(input!=null) {
-			input.replaceAll("\\s+", "");
-			for(int i=0;i<input.length();i++) {
-				if(!count.containsKey(input.charAt(i))) {
-					count.put(input.charAt(i), 0);
-				}
-				else {
-					count.put(input.charAt(i), count.get(input.charAt(i))+1);
-				}
-			}
-			Stream<Entry<Character,Integer>> sorted=count.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()));
-			Stream<Entry<Character,Integer>> sortedv=count.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()));
-			res=sorted.findFirst().get().getKey();
-			int val=sortedv.findFirst().get().getValue();
-			if(val<1) {
-				System.out.println("No characters repeated");
-				return 0;
-			}
-		}
-		else{
-			System.out.println("No characters repeated");
-			return 0;
-		}
-		System.out.println(res);
-		return res;
+		HashMap<Character, Integer> charCount=new HashMap<>();
+        if(input==null||input.isEmpty()) {
+                        return '0';
+        }
+        char[] charArray=input.toCharArray();
+       
+        for (char c : charArray) {
+                        if(charCount.containsKey(c)) {
+                                        charCount.put(c, charCount.get(c)+1);
+                        }
+                        else {
+                                        charCount.put(c, 1);
+                        }
+        }
+       
+        int maxCount=0;
+        char maxChar = 0;
+        for (Map.Entry<Character, Integer> countCheck : charCount.entrySet()) {
+                        if(countCheck.getValue()>maxCount) {
+                                        maxCount=countCheck.getValue();
+                                        maxChar=countCheck.getKey();
+                        }
+                        else if(countCheck.getValue()==maxCount) {
+                                        if(countCheck.getKey()<maxChar) {
+                                                        maxCount=countCheck.getValue();
+                                                        maxChar=countCheck.getKey();
+                                        }
+                        }
+        }
+        if(maxCount==1) {
+                        return '0';
+        }
+        return maxChar;
 	}
 }
